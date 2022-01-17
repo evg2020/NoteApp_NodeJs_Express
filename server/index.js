428f08b1d91941b69dfc88b731df1bb4ebba5cc2
Your Name <you@example.com>
2022-01-17 11:33:00 +0300

    Noedejs + Express

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+app.use('/api/posts', posts);
+
+const port = process.env.PORT || 5000;
+ app.listen(port, () => console.log(`server started on port ${port}`))
---
+++ server/routes/api/posts.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const mongodb = require("mongodb");
+
+const router = express.Router();
+
+//get posts
+router.get('/', async (req, res) => {
+  // res.send([{'_id': '61e137a23486b6843f577303', 'text' :'post'}]);
+  const posts = await loadCollections();
+  res.send(await posts.find().toArray());
+});
+
+//get posts by id
+ router.get('/:id', async (req, res)=>{
+  const posts = await loadCollections();
+  res.send(await posts.find({_id : new mongodb.ObjectID(req.params.id)}.toArray()));
+ });
+
+//add posts
+router.post('/', async (req, res) => {
+  const posts = await loadCollections();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date(),
+  });
+  res.status(201).send();
+});
+
+//delete posts
+router.delete('/:id', async (req, res) => {
+  const posts = await loadCollections();
+  await posts.deleteOne({  _id: new mongodb.ObjectID(req.params.id)
+  });
+  res.status(200).send();
+})
+
+
+async function loadCollections() {
+  const client = await mongodb.MongoClient.connect('mongodb+srv://user:bashneft@cluster0.pjbir.mongodb.net/listPost/', {
+    useNewUrlParser: true
+  });
+  return client.db('listPost').collection('ports');
+}
+
+
+
+module.exports = router;
